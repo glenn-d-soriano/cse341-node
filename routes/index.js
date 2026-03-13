c83532ada3e33e3464a92666f7804db1e853f930
glenn-d-soriano <glenndannielsoriano@gmail.com>
2026-03-12 23:53:51 -0500

    add swagger route to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json'); 
 
 const professionalController = require('../controllers/professional');
 
+// Swagger Route
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
